4a0dea07ddd9023b0228661e8f914f581c8a2a47
oleg <hulktv222@mail.ru>
2021-10-27 17:55:14 +0300

    make it better

--- medClinic/settings.py
+++ medClinic/settings.py
@@ -24,8 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure--cnz1+et#q%pr@)fo1q7nk0t&p7+z*2+kf4&600!)7&8l-$#3g')
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
