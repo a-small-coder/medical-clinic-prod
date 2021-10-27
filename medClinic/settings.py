dbb37c568f44a25c6ebad280e0767d72c367005e
oleg <hulktv222@mail.ru>
2021-10-27 17:48:32 +0300

    change secret key couple

--- medClinic/settings.py
+++ medClinic/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure--cnz1+et#q%pr@)fo1q7nk0t&p7+z*2+kf4&600!)7&8l-$#3g')
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure--cnz1+et#q%pr@)fo1q7nk0t&p7+z*2+kf4&600!)7&8l-$#3g')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
